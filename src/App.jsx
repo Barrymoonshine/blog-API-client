4c64d5821a36c5a131372e9db23e855aee740df9
Nick Sheward <nickshwrd@gmail.com>
2023-08-23 16:02:12 +0100

    Created initial Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <>
-      <div>
-        <h3>Hello World</h3>
-      </div>
+      <Header />
     </>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h3>Sayonara - Link to Home page</h3>
+        <h4>Region - Drop down to view by Region </h4>
+      </div>
+      <nav>
+        <ul>
+          <li>Register - TBC</li>
+          <li>Log in - TBC</li>
+          <li>My Account - TBC</li>
+          <li>Create - TBC, Page</li>
+          <li>Log out - TBC, button</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
