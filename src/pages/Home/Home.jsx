c8ce7a12859c7231567cd5ff1718b1a2ddaaf9b6
Nick Sheward <nickshwrd@gmail.com>
2023-08-29 08:59:20 +0100

    Welcome container added and styled on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,14 +9,27 @@ const Home = () => {
     data = [],
   } = useFetch('https://ancient-water-2934.fly.dev/blogs', { method: 'GET' });
 
-  if (error) return <p>Error! {error}</p>;
-  if (loading) return <p>Loading...</p>;
-
   return (
-    <div>
-      <h4>Sayonara</h4>
-      <p>Create the travel blog of your dreams</p>
-      <p>Photo by Annie Spratt</p>
+    <div className='home-container'>
+      <div className='welcome-container'>
+        <div>
+          <h4>Want to create the travel blog of your dreams?</h4>
+          <p>
+            Sign up for a free account today, to create and edit your very own
+            travel blog!
+          </p>
+          <button className='register-button'>Register</button>
+        </div>
+        <div>
+          <img
+            className='welcome-image'
+            src='./images/travel-blog.jpg'
+            alt='travel blog'
+          />
+        </div>
+      </div>
+      {error && <p>Error! {error}</p>}
+      {loading && <p>Loading...</p>}
       {data &&
         data.map((item) => (
           <Blog
