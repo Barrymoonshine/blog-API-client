c8b329bb9c3d9b7a02b318cb4d66272cb63a4208
Nick Sheward <nickshwrd@gmail.com>
2023-08-29 14:48:29 +0100

    Added front end check that passwords match

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -9,6 +9,7 @@ const Register = () => {
     handleSubmit,
     reset,
     formState: { errors },
+    watch,
   } = useForm();
 
   const [successMessage, setSuccessMessage] = useState(null);
@@ -52,7 +53,7 @@ const Register = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor='Username'> Username:</label>
           <input {...register('username', { required: true })} />
-          {errors.title && <span>This field is required</span>}
+          {errors.username && <span>This field is required</span>}
 
           <label htmlFor='password'> Password:</label>
           <input
@@ -63,13 +64,27 @@ const Register = () => {
                 /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,20}$/,
             })}
           />
-          {errors.caption && (
+          {errors.password && (
             <span>
               Please enter a password that is between 8 and 20 characters long
               and contains at least one number, one capital letter and one
               special symbol(!@#$%^&*=+-_)
             </span>
           )}
+
+          <label htmlFor='password'> Confirm password:</label>
+          <input
+            type='password'
+            {...register('confirmPassword', {
+              required: true,
+              validate: (value) => {
+                if (watch('password') != value) {
+                  return 'Passwords do no match';
+                }
+              },
+            })}
+          />
+          {errors.confirmPassword && <span>Passwords do not match</span>}
 
           {errorMessage && (
             <div>
