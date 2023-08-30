dfe5b82f48cf92dd4ffb81bcce3781073c22c8e9
Nick Sheward <nickshwrd@gmail.com>
2023-08-30 15:45:53 +0100

    Moved useEffect into useAuth custom hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,22 @@
 import { useReducer } from 'react';
 import authReducer, { initialState } from '../state/authReducer';
+import { useEffect } from 'react';
+import ACTIONS from '../utils/ACTIONS';
 
 const useAuth = () => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    // Check for an existing user in local storage on page load
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      console.log('existing user in local storage', user);
+      dispatch({
+        type: ACTIONS.LOG_IN,
+        payload: { user },
+      });
+    }
+  }, []);
 
   return {
     state,
