43ba368ba6f6aed47bd9ef68d6ead1dfcad0ea84
Nick Sheward <nickshwrd@gmail.com>
2023-08-31 08:26:17 +0100

    Created LogIn page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,56 @@
 import './LogIn.css';
+import { useForm } from 'react-hook-form';
+import useCreateAuth from '../../hooks/useCreateAuth';
 
 const LogIn = () => {
-  return <div>Hello this is the Log In page</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const { createAuth, error, isLoading } = useCreateAuth();
+
+  const onSubmit = async (formData) => {
+    await createAuth('https://ancient-water-2934.fly.dev/user/log-in', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='Username'> Username:</label>
+        <input {...register('username', { required: true })} />
+        {errors.username && <span>This field is required</span>}
+
+        <label htmlFor='password'> Password:</label>
+        <input
+          type='password'
+          {...register('password', {
+            required: true,
+            pattern:
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,20}$/,
+          })}
+        />
+        {errors.password && (
+          <span>
+            Please enter a password that is between 8 and 20 characters long and
+            contains at least one number, one capital letter and one special
+            symbol(!@#$%^&*=+-_)
+          </span>
+        )}
+
+        {error && <span className='error'>{error.message}</span>}
+        <button disabled={isLoading}> Log in </button>
+      </form>
+    </>
+  );
 };
 
 export default LogIn;
