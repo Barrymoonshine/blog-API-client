b68ac241b4aa8d1445b3028b58218e3040df604d
Nick Sheward <nickshwrd@gmail.com>
2023-09-19 15:58:35 +0100

    Added log to Home to help with debugging vercel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
   const topThreeLikedBlogs =
     blogs && likes && getTopThreeLikedBlogs(blogs, likes);
 
+  console.log('blogsError', blogsError);
+
   return (
     <div className='home-container'>
       <div className='welcome-container'>
