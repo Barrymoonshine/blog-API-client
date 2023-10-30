6ed63518fd4ef5d54057c1be715e0018692ac638
Nick Sheward <nickshwrd@gmail.com>
2023-10-30 14:40:05 +0000

    Added region menu drop down test

--- tests/Header.test.jsx
+++ tests/Header.test.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import Header from '../src/components/Header/Header';
 import MainRouter from '../src/components/MainRouter/MainRouter';
+import useAuthDispatch from '../src/hooks/useAuthDispatch';
 
 describe('Header component', () => {
   const wrappedHeader = (
@@ -37,6 +38,18 @@ describe('Header component', () => {
     expect(screen.getByText('Log in').closest('a')).toBeInTheDocument();
   });
 
+  it('renders region drop down menu after button click', async () => {
+    const user = userEvent.setup();
+
+    // Render component with Routing to test Routing works correctly
+    render(wrappedHeader);
+    const regionButton = screen.getByRole('button', { value: 'Region ⌄' });
+
+    await user.click(regionButton);
+
+    expect(screen.getByText('EXPLORE A REGION')).toBeInTheDocument();
+  });
+
   it('renders log in page after link click', async () => {
     const user = userEvent.setup();
 
@@ -57,4 +70,16 @@ describe('Header component', () => {
       screen.getByText('Welcome back fellow traveler')
     ).toBeInTheDocument();
   });
+
+  // it('renders the log out link for logged in users', () => {
+  //   // How can I set the default value of true
+
+  //   vi.mock('../src/hooks/useAuthDispatch');
+
+  //   render(wrappedHeader);
+
+  //   useAuthDispatch().logIn();
+
+  //   expect(screen.getByText('Log out')).toBeInTheDocument();
+  // });
 });
