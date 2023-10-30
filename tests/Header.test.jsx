668b1ead9f125c35105e9ebed523b594b5f128b0
Nick Sheward <nickshwrd@gmail.com>
2023-10-30 10:26:33 +0000

    Added and passed tests for Log In link in Header

--- tests/Header.test.jsx
+++ tests/Header.test.jsx
@@ -1,30 +1,60 @@
 import { render, screen } from '@testing-library/react';
 import { AppProvider } from '../src/context/AppContext';
 import { BrowserRouter } from 'react-router-dom';
-
+import userEvent from '@testing-library/user-event';
 import Header from '../src/components/Header/Header';
+import MainRouter from '../src/components/MainRouter/MainRouter';
 
 describe('Header component', () => {
+  const wrappedHeader = (
+    <BrowserRouter>
+      <AppProvider>
+        <Header />
+      </AppProvider>
+    </BrowserRouter>
+  );
+
   it('renders region button correctly', () => {
-    render(
-      <BrowserRouter>
-        <AppProvider>
-          <Header />
-        </AppProvider>
-      </BrowserRouter>
-    );
+    render(wrappedHeader);
     expect(screen.getByRole('button').textContent).toMatch(/Region ⌄/i);
   });
 
   it('renders page title and home anchor link correctly', () => {
+    render(wrappedHeader);
+
+    expect(screen.getByText('Sayonara').closest('a')).toBeInTheDocument();
+  });
+
+  it('renders page title and home anchor link correctly', () => {
+    render(wrappedHeader);
+
+    expect(screen.getByText('Sayonara').closest('a')).toBeInTheDocument();
+  });
+
+  it('renders log in anchor link correctly', () => {
+    render(wrappedHeader);
+
+    expect(screen.getByText('Log in').closest('a')).toBeInTheDocument();
+  });
+
+  it('renders log in page after link click', async () => {
+    const user = userEvent.setup();
+
+    // Render component with Routing to test Routing works correctly
     render(
       <BrowserRouter>
         <AppProvider>
           <Header />
+          <MainRouter />
         </AppProvider>
       </BrowserRouter>
     );
+    const logInLink = screen.getByText('Log in').closest('a');
 
-    expect(screen.getByText('Sayonara').closest('a')).toBeInTheDocument();
+    await user.click(logInLink);
+
+    expect(
+      screen.getByText('Welcome back fellow traveler')
+    ).toBeInTheDocument();
   });
 });
